2805df969b8d6ee29cb95df09f4a62220d4faf4f
santhosh <santhosh@santhosh-PC>
2018-10-14 21:56:23 +0530

    commit for sample project2

--- sample_automation/src/sample_automation/testClass.java
+++
@@ -1,9 +0,0 @@
-package sample_automation;
-
-public class testClass {
-	
-	public static void main(String[] args)
-	{
-		System.out.println("new modifications");
-	}
-}
---
+++ sampleproject2/src/sampleproject2/testsample2.java
@@ -0,0 +1,12 @@
+package sampleproject2;
+
+public class testsample2 
+{
+	
+	
+	public static void main(String args[])
+	{
+		System.out.println("test sample2");
+	}
+
+}
